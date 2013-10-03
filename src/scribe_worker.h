efa3702d49ae4efde78a75bd7eeb7fc80a7bc17a
thinker0 <thinker0@gmail.com>
2013-10-03 11:18:42 +0900

    thread-pool

---
+++ src/scribe_worker.h
@@ -0,0 +1,14 @@
+//
+//  scribe_worker.h
+//  
+//
+//  Created by thinker0 on 13. 10. 3..
+//
+//
+
+#ifndef ____scribe_worker__
+#define ____scribe_worker__
+
+#include <iostream>
+
+#endif /* defined(____scribe_worker__) */
